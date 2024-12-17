88acea0c39d43fb5b3c8d8047af3ebac316d6b60
tanmaydixit27 <82702135+tanmaydixit27@users.noreply.github.com>
2024-12-17 22:21:14 +0530

    Update api.js

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'https://annadaata-backend-xtzg.onrender.com', 
+  baseURL: 'annadaata-backend-xtzg.onrender.com', 
   headers: {
     'Content-Type': 'application/json',
   },
